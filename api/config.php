98ea08bba58cd2dc12e3c50b9c80a357f399844e
harperrp <124433217+harperrp@users.noreply.github.com>
2026-05-11 04:13:38 -0300

    att

--- api/config.php
+++ api/config.php
@@ -5,9 +5,9 @@
  */
 return [
     'db_host' => getenv('VG_DB_HOST') ?: 'localhost',
-    'db_name' => getenv('VG_DB_NAME') ?: 'prefeitura_vgrp',
-    'db_user' => getenv('VG_DB_USER') ?: 'db_user',
-    'db_pass' => getenv('VG_DB_PASS') ?: 'db_senha',
+    'db_name' => getenv('VG_DB_NAME') ?: 'banco2vgm',
+    'db_user' => getenv('VG_DB_USER') ?: 'uservgm2',
+    'db_pass' => getenv('VG_DB_PASS') ?: 'Vargemgrande@2026',
     'db_charset' => 'utf8mb4',
     'session_name' => 'VG_PORTAL_ADMIN',
 ];
